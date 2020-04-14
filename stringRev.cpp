6cdced854a6beef0dbf04bfb5ccb474ea69b37e6
Karan Hejmadi <hejmadikaran9@gmail.com>
2020-04-14 16:24:25 +0530

    string shift

--- stringRev.cpp
+++ stringRev.cpp
@@ -1,26 +1,26 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void reverseString(vector<char>& str)
+void reverseString(vector<char> &str)
 {
-    for(int i=0 ;i != str.size()/2;i++)
+    for (int i = 0; i != str.size() / 2; i++)
     {
-        swap(str[i], str[str.size()-(i+1)]);
+        swap(str[i], str[str.size() - (i + 1)]);
     }
 }
 
 int main()
 {
     string word;
-    cout<<"String: ";
-    cin>>word;
+    cout << "String: ";
+    cin >> word;
     vector<char> str(word.begin(), word.end());
-    cout<<"Reversed string is ";
-    stringReverse(str);
+    cout << "Reversed string is ";
+    reverseString(str);
     vector<char>::iterator it;
-    for(it=str.begin(); it!=str.end();++it)
+    for (it = str.begin(); it != str.end(); ++it)
     {
-        cout<<*(it);
+        cout << *(it);
     }
 }
